bfd14bc84c1ceccf778da48294f988c83ea30662
金澤圭人 <keitodayo.@0107.icloud.com>
2024-06-25 10:40:19 +0900

    13やりました

--- shootig/src/GameFrame.java
+++ shootig/src/GameFrame.java
@@ -4,20 +4,45 @@ public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);
+		GameWorld.stage=1;
+		GameWorld.score=0;
+		while(true) {
+			GameWorld.player.x=100;
+			GameWorld.player.y=300;
 		GameWorld.playerBullets=new Vector<PlayerBullet>();
 		GameWorld.enemies=new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+		GameWorld.enemies.add(new EnemyBase(100,50,GameWorld.stage,0));
 		while(true) {
 			clear();
+			drawString("Stage = "+GameWorld.stage,300,50,15);
+			drawString("score="+GameWorld.score,300,80,15);
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
 			movePlayerBullets();
 			moveEnemies();
 			checkPlayerAndEnemies();
 			checkPlayerBulletsAndEnemies();
+			if(GameWorld.enemies.size()==0) {
+				setColor(0,0,0);
+				drawString("クリア!",100,200,40);
+				if(GameWorld.enterPressed) {
+					GameWorld.stage++;
+					break;
+				}
+			}else if(GameWorld.player.y<0) {
+				setColor(0,0,0);
+				drawString("ゲームオーバー！",50,200,40);
+				if(GameWorld.enterPressed) {
+					GameWorld.stage=1;
+					GameWorld.score=0;
+					break;
+				}
+			}
 			sleep(0.03);
 		}
 	}
+	
+}
 			public void checkPlayerAndEnemies() {
 				for(int i=0; i<GameWorld.enemies.size();i++) {
 					Enemy e=GameWorld.enemies.get(i);
@@ -33,6 +58,15 @@ public class GameFrame extends MyFrame {
 		   Enemy e=GameWorld.enemies.get(i);
 		   e.draw(this);
 		   e.move();
+		}
+		int i=0;
+		while(i<GameWorld.enemies.size()) {
+			Enemy e=GameWorld.enemies.get(i);
+			if(e.y>400) {
+				GameWorld.enemies.remove(i);
+			}else {
+				i++;
+			}
 		}
 	}
 	public void movePlayerBullets() {
@@ -62,6 +96,7 @@ public void checkPlayerBulletsAndEnemies() {
 				e.life--;
 			}
 			if(e.life<=0) {
+				GameWorld.score+=e.score;
 				GameWorld.enemies.remove(j);
 			}else {
 				j++;
@@ -75,7 +110,7 @@ public void checkPlayerBulletsAndEnemies() {
 	}
 }
 public boolean checkHit(Character a,Character b) {
-	if(Math.abs(a.x-b.x)<=30 && Math.abs(a.y-b.y)<=30) {
+	if(Math.abs(a.x-b.x)<=20 && Math.abs(a.y-b.y)<=20) {
 		return true;
 	}else {
 		return false;
